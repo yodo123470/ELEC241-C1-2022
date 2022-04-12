83e62f7f7dc8d7adde55cfe6f50b6e15e04acce7
yodo123470 <Aamer.dot@hotmail.com>
2022-04-12 19:25:38 +0100

    task1 clean up

--- task1a.sv
+++ task1a.sv
@@ -15,19 +15,6 @@ A	B	C	Y
 
 module task1a(output logic [1:0] Y, input logic A, B, C);
 
-//if (A == 0 && B== 0 && C == 0)
-//	Y = 0; 
-//
-//else if (A == 0 && B == 0 && C== 1) || (A == 0 && B == 1 && C== 0) || (A == 1 && B == 0 && C== 0)
-//	Y = 1;
-//
-//else if (A == 0 && B == 1 && C== 1) || (A == 1 && B == 0 && C== 1) || (A == 1 && B == 1 && C== 0)
-//	Y = 2;
-//
-//else
-//	Y = 3;
-//	
-	
 
 //Internal wires
 wire Ainv;
@@ -60,35 +47,6 @@ and u9 (term2, A, invB, invC);
 and u10 (term3, A, B, C);
 
 or u11 (Y[0], term0, term1, term2, term3);
-
-
-
-
-
-
-//
-//wire term00;
-//wire term11;
-//wire term22;
-//wire term33;
-//
-//
-//
-////Place and connect gates
-//
-////not u12 (invA, A);
-////not u13 (invB, B);
-//////not u13 (invA, A);
-////not u14 (invC, C);
-//////not u15 (invB, B);
-//////not u16 (invC, C);
-//
-//and u18 (term00, invA, B, C);
-//and u19 (term11, A, invB, C);
-//and u20 (term22, A, B, invC);
-//and u21 (term33, A, B, C);
-//
-//or u22 (Y[1], term00, term11, term22, term33);
 
 
 
